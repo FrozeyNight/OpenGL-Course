3ecf02c65e8b4f00aa47e69b865c3246c627954e
FrozeyNight <frozeynight@gmail.com>
2026-03-29 20:42:22 +0200

    Learned about Vertex Buffers and a little bit about Drawing in OpenGL

--- src/Application.cpp
+++ src/Application.cpp
@@ -27,17 +27,38 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    
+    // Creating 1 buffer and sending it's ID to the "buffer" variable
+    glGenBuffers(1, &buffer);
+    // everything in openGL has an ID (just an integer, 0 usually means bad)
+    // so if you want to make a triangle for example you make 1 buffer for it and then pass it's ID every time you want to draw it
+
+    // selecting in openGL is called binding
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    // "target" means what is the purpose of this (here just an array)
+
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    // target, size of the buffer in bytes, pointer to the buffer, STATIC(made once but called a lot of times) DRAW(well we want to draw it)
+    // the STATIC and DRAW are just hints, so if you for example use STATIC and modify it during runtime it will still work, but will be slower
+    // also good documentation docs.GL
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        // This draws the currently bound buffer (so the one we specified before this loop)
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // If we would bind a different buffer, it would use that one instead
+        // That's why we don't pass the buffer to this function
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
