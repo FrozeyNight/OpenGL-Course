02c96dd70d04f3eee687e1712e058cf8869b1fe2
FrozeyNight <frozeynight@gmail.com>
2026-03-30 18:48:20 +0200

    Learned about Vertex Attributes, Layouts and the basics of how Shaders work in OpenGL

--- src/Application.cpp
+++ src/Application.cpp
@@ -33,6 +33,8 @@ int main(void)
          0.5f, -0.5f
     };
 
+    // openGL is a state machine, which means that you set states and enable or disable them (for the most part) 
+
     unsigned int buffer;
     
     // Creating 1 buffer and sending it's ID to the "buffer" variable
@@ -48,6 +50,24 @@ int main(void)
     // target, size of the buffer in bytes, pointer to the buffer, STATIC(made once but called a lot of times) DRAW(well we want to draw it)
     // the STATIC and DRAW are just hints, so if you for example use STATIC and modify it during runtime it will still work, but will be slower
     // also good documentation docs.GL
+
+    // enables vertex attributes
+    glEnableVertexAttribArray(0);
+    // in openGL order doesn't matter as long as you bind the buffer you're working on
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void*)0);
+    // index is where it should be in the "array" of the shader, since it calls them more or less one by one
+    // size is basically in how many dimensions you want to draw something (i think) (how many attributes in one vertex?)
+    // type - in what variable type is the data
+    // do you want to normalize the data to a float (I think)
+    // stride is how many bytes does it have to jump to get to another vertex
+    // pointer is how many bytes does it have to jump to get to this attribute in each vertex
+    // the pointer has to be well a const pointer, so you have to cast it (0 specifically doesn't but figured I should show it here)
+
+    // the vertex shader is called first and determines where exactly to put each vertex on the screen and prepares attributes for the pixel shader (primarily, it does other stuff too)
+    // the fragment (or pixel) shader rastersizes the image (actually draws and chooses the color of every pixel on screen). Gets called for each pixel on screen
+    // because the fragment shader may be called millions of times you should optimize it and do most heavy calculations in the vertex shader
+    // since you can pass data from the vertex shader to the pixel shader
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
